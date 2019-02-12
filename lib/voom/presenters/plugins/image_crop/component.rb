282d9cb05c75522558bf65358dda885746dbc56f
diamond-shine <focusingdev@outlook.com>
2019-02-11 22:21:24 -0500

    attrs

--- lib/voom/presenters/plugins/image_crop/component.rb
+++ lib/voom/presenters/plugins/image_crop/component.rb
@@ -5,6 +5,8 @@ module Voom
     module Plugins
       module ImageCrop
         class Component < DSL::Components::Image
+          attr_reader :crop_attrs
+
           def initialize(**attribs_, &block)
             @crop_attrs = %w(crop_x crop_y crop_width crop_height)
             super(type: :image_crop, **attribs_, &block)
