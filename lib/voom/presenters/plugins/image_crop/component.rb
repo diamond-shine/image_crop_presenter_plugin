c5e9ff0e33aeb2df0c6ed696188874897829206d
diamond-shine <focusingdev@outlook.com>
2019-02-11 21:11:56 -0500

    adjusting instance vars

--- lib/voom/presenters/plugins/image_crop/component.rb
+++ lib/voom/presenters/plugins/image_crop/component.rb
@@ -6,8 +6,8 @@ module Voom
       module ImageCrop
         class Component < DSL::Components::Image
           def initialize(**attribs_, &block)
-            super(type: :image_crop, **attribs_, &block)
             @crop_attrs = %w(crop_x crop_y crop_width crop_height)
+            super(type: :image_crop, **attribs_, &block)
           end
         end
       end
