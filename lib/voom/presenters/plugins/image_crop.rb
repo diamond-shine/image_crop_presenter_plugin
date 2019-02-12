9a9db66453e2a697727eac73e8637d5498b3f210
diamond-shine <focusingdev@outlook.com>
2019-02-11 21:40:28 -0500

    adjusting image crop plugin

--- lib/voom/presenters/plugins/image_crop.rb
+++ lib/voom/presenters/plugins/image_crop.rb
@@ -5,8 +5,17 @@ module Voom
     module Plugins
       module ImageCrop
         module DSLComponents
-          def image_crop(**attributes, &block)
+          def image_cropper(**attributes, &block)
             self << ImageCrop::Component.new(**attributes, &block)
+          end
+
+          module WebClientComponents
+            def render_image_crop(comp, render:, components:, index:)
+              view_dir = File.join(__dir__, 'image_crop')
+              render.call :erb, :image_crop, views: view_dir,
+                        locals: {comp: comp,
+                                 components: components, index: index}
+            end
           end
         end
       end
