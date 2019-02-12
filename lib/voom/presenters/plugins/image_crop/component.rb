890646c217a5187427ea2eed76ccbcebc195b7fa
diamond-shine <focusingdev@outlook.com>
2019-02-11 22:36:47 -0500

    moar adjustments

--- lib/voom/presenters/plugins/image_crop/component.rb
+++ lib/voom/presenters/plugins/image_crop/component.rb
@@ -10,7 +10,6 @@ module Voom
           def initialize(**attribs_, &block)
             @crop_attrs = %w(crop_x crop_y crop_width crop_height)
             super(type: :image_crop, **attribs_, &block)
-            expand!
           end
         end
       end
