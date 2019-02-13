b9e253c4fd1933580b9d873e41f8d6b093e9b8a9
diamond-shine <focusingdev@outlook.com>
2019-02-12 21:31:02 -0500

    adjusting module code

--- lib/voom/presenters/plugins/image_crop.rb
+++ lib/voom/presenters/plugins/image_crop.rb
@@ -8,14 +8,14 @@ module Voom
           def image_crop(**attributes, &block)
             self << ImageCrop::Component.new(parent: self, **attributes, &block)
           end
+        end
 
-          module WebClientComponents
-            def render_image_crop(comp, render:, components:, index:)
-              view_dir = File.join(__dir__, 'image_crop')
-              render.call :erb, :image_crop, views: view_dir,
-                        locals: {comp: comp,
-                                 components: components, index: index}
-            end
+        module WebClientComponents
+          def render_image_crop(comp, render:, components:, index:)
+            view_dir = File.join(__dir__, 'image_crop')
+            render.call :erb, :component, views: view_dir,
+                      locals: {comp: comp,
+                                components: components, index: index}
           end
         end
       end
