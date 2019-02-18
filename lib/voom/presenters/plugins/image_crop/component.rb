0e844bfcd28180fb97a19a4dff3950d00355c35c
diamond-shine <focusingdev@outlook.com>
2019-02-17 22:37:16 -0500

    adding support for all croppr options

--- lib/voom/presenters/plugins/image_crop/component.rb
+++ lib/voom/presenters/plugins/image_crop/component.rb
@@ -5,11 +5,28 @@ module Voom
     module Plugins
       module ImageCrop
         class Component < DSL::Components::Image
-          attr_reader :crop_attrs
+          attr_accessor :aspect_ratio, :max_width, :max_height, :min_width, :min_height, :size_unit
 
           def initialize(**attribs_, &block)
-            @crop_attrs = %w(crop_x crop_y crop_width crop_height)
             super(type: :image_crop, **attribs_, &block)
+
+            @aspect_ratio = attribs.delete(:aspect_ratio)
+            @max_width = attribs.delete(:max_width)
+            @max_height = attribs.delete(:max_height)
+            @min_width = attribs.delete(:min_width)
+            @min_height = attribs.delete(:min_height)
+            @size_unit = attribs.delete(:size_unit)
+          end
+
+          def default_component_data
+            {
+              "data-aspect-ratio": aspect_ratio,
+              "data-max-width": max_width,
+              "data-max-height": max_height,
+              "data-min-width": min_width,
+              "data-min-height": min_height,
+              "data-size-unit": size_unit
+            }.compact
           end
         end
       end
